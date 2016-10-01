357f164a538fdeab425927e8d8b883dbc73c81ee
Brenda Wallace <brenda@wallace.net.nz>
2016-10-01 16:06:01 +1300

    relationships between models

--- app/src/main/java/nz/allthethings/wharehauora/models/Home.java
+++ app/src/main/java/nz/allthethings/wharehauora/models/Home.java
@@ -1,8 +1,10 @@
 package nz.allthethings.wharehauora.models;
 
+import io.realm.RealmList;
 import io.realm.RealmObject;
 
 public class Home  extends RealmObject {
     private String name;
     private User owner;
+    public RealmList<Sensor> sensors;
 }
--- app/src/main/java/nz/allthethings/wharehauora/models/Sensor.java
+++ app/src/main/java/nz/allthethings/wharehauora/models/Sensor.java
@@ -1,8 +1,10 @@
 package nz.allthethings.wharehauora.models;
 
+import io.realm.RealmList;
 import io.realm.RealmObject;
 
 public class Sensor extends RealmObject {
     private Home home;
     private String room_name;
+    public RealmList<Reading> readings;
 }
